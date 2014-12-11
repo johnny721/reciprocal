c66114002903f3d998c3e53220287d0632754ca3
Wonhyeong Chae <beoahn@gmail.com>
2014-12-11 17:23:07 -0400

    Fix spelling mistake

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 			<p class="styled_box" id="footer-info">
-			&copy; <?php echo date("Y"); ?> Wonhyeong Chae, Kaiden Hiew, John Miahilescu
+			&copy; <?php echo date("Y"); ?> Wonhyeong Chae, Kaiden Hiew, John Mihailescu
 			</p>
 		</div>
 	</body>
